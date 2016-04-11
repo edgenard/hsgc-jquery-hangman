0323e7461bff641758e4d2dfa5e2d1495e47d3ee
Emmanuel Genard <edgenard@gmail.com>
2016-04-11 10:27:10 -0400

    Moved some game business logic into other file

--- scripts/hangman-codepen.js
+++ scripts/hangman-codepen.js
@@ -1,88 +1,33 @@
 $().ready(function () {
-  if (typeof window.Hangman === "undefined") {
-    window.Hangman = {}
-  }
-
-  var NAMES = [
-    "Emmanuella",
-    "Dishelle",
-    "Patience",
-    "Travis",
-    "Jemar",
-    "Tia",
-    "Garyl",
-    "Mohamed",
-    "Maximillian",
-    "Zaire",
-    "Araceli",
-    "Ashley",
-    "Trevon"
-  ]
-
-  var IMAGES = [
-    "http://i.imgur.com/WvCmM8b.png",
-    "http://i.imgur.com/r9Zy7yN.png",
-    "http://i.imgur.com/ZjkBARC.png",
-    "http://i.imgur.com/gFTW0Ta.png",
-    "http://i.imgur.com/j5o8met.png",
-    "http://i.imgur.com/ma29Fvx.png",
-    "http://i.imgur.com/s9QC4RI.png",
-  ]
-
-  var secretWord;
-  var imageIndex = 0;
-
-  var hangman = window.Hangman;
-
-  var findIndexes = function(value, arr) {
-    var indexes = [];
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === value) {
-        indexes.push(i)
-      }
-    }
-    return indexes;
-
-  }
-
-  var pickWord = function() {
-    var randIndex = Math.floor(Math.random() *(hangman.names.length))
-    secretWord = hangman.names[randIndex].toLowerCase().split("")
-    hangman.names.splice(randIndex,1)
-    return secretWord;
-  }
 
 
 
-  hangman.names = $.extend([], NAMES);
 
-  //Setup Intro UI
-  $(".number-of-names p").text("There are " + hangman.names.length + " more words to play.")
 
   var welcomeWord = "ClickPlayaword";
 
-  hangman.setupGame = function () {
-    imageIndex = 0;
-    $(".blank-spaces").empty();
-    var word = pickWord();
+  var setupGame = function () {
+    resetImages()
+    $(".blank-spaces").empty(); //This empties out all the elements in the .blank-spaces section
+    var word = pickWord();// Picks a word from secret list of possible words
     word.forEach(function (letter, index) {
       $(".blank-spaces").append("<span id=" + index +"></span?")
-    })
+    })// This creates the blank spaces for the world.
 
     $(".letters button").show()
     $(".letters button").click(letterGuess)
-    $(".number-of-names p").text("There are " + hangman.names.length + " more words to play.")
+    $(".game-messages p").text("There are " + possibleWords.length + " more words to play.")
     $(".hangman-image img").replaceWith("<img src='http://i.imgur.com/O5KepLs.png' alt='hangman image'>")
   }
 
-  hangman.resetGame = function () {
-    imageIndex = 0;
-    hangman.names = $.extend([], NAMES);
+  var resetGame = function () {
+    resetImages()
+    possibleWords = $.extend([], NAMES);
     $(".blank-spaces").empty()
     welcomeWord.split("").forEach(function (letter) {
       $(".blank-spaces").append("<span>" + letter + "</span?")
     })
-    $(".number-of-names p").text("There are " + hangman.names.length + " more words to play.")
+    $(".game-messages p").text("Welcome to HSGC JQuery Hangman. Click \"Play a Word\" to start playing!")
 
     $(".hangman-image img").replaceWith("<img src='http://i.imgur.com/O5KepLs.png' alt='hangman image'>")
 
@@ -96,7 +41,7 @@ var letterGuess = function (event) {
       fillInBlanks(letter) //fill In the blank spaces with the letter
     if(winner()){//Check if they've won
       $(".letters button").off("click")
-      $(".number-of-names p").text("You Won! Click Play a Word to play again")
+      $(".game-messages p").text("You Won! Click Play a Word to play again")
     }
     }else{
       if(imageIndex < 6){//Check if they have more chances
@@ -104,7 +49,7 @@ var letterGuess = function (event) {
         imageIndex += 1;
       } else {
         $(".hangman-image figure img").replaceWith("<img src=" + IMAGES[imageIndex] +" alt='hangman image'>")
-        $(".number-of-names p").text("Game Over. Click Play Word to play again")
+        $(".game-messages p").text("Game Over. Click Play Word to play again")
         $(".letters button").off("click")
         revealWord()
       }
@@ -144,8 +89,8 @@ var letterGuess = function (event) {
       })
     })
   }
-  $(".play").click(hangman.setupGame)
+  $(".play").click(setupGame)
 
-  $(".reset").click(hangman.resetGame)
+  $(".reset").click(resetGame)
 
 })
---
+++ scripts/utilities-and-constants.js
@@ -0,0 +1,55 @@
+var NAMES = [
+  "Emmanuella",
+  "Dishelle",
+  "Patience",
+  "Travis",
+  "Jemar",
+  "Tia",
+  "Garyl",
+  "Mohamed",
+  "Maximillian",
+  "Zaire",
+  "Araceli",
+  "Ashley",
+  "Trevon"
+]
+
+var IMAGES = [
+  "http://i.imgur.com/WvCmM8b.png",
+  "http://i.imgur.com/r9Zy7yN.png",
+  "http://i.imgur.com/ZjkBARC.png",
+  "http://i.imgur.com/gFTW0Ta.png",
+  "http://i.imgur.com/j5o8met.png",
+  "http://i.imgur.com/ma29Fvx.png",
+  "http://i.imgur.com/s9QC4RI.png",
+]
+
+
+var secretWord;
+var imageIndex = 0;
+
+var findIndexes = function(value, arr) {
+  var indexes = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      indexes.push(i)
+    }
+  }
+  return indexes;
+
+}
+
+
+var pickWord = function() {
+  var randIndex = Math.floor(Math.random() *(possibleWords.length))
+  secretWord = possibleWords[randIndex].toLowerCase().split("")
+  possibleWords.splice(randIndex,1)
+  return secretWord;
+}
+
+
+var possibleWords = $.extend([], NAMES)
+
+var resetImages = function () {
+  imageIndex = 0;
+}
